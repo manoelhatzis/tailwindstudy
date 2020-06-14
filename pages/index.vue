7d779319f6dfc080135987fc134483bd255baed3
Manoel Soares Hatzis <hatzapio@gmail.com>
2020-06-13 23:15:14 -0300

    Commit inicial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="text-6xl text-purple-700">Hello from Tailwind</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {}
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
